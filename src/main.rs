f019c9551ff008e79e619f3c7fbd81da3823fbf6
Jason Dagit <dagit@galois.com>
2020-05-22 23:40:21 -0700

    Add example based on generators

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(generators, generator_trait)]
 #![allow(dead_code)]
 const MEM_SIZE: usize = 65536;
 
@@ -219,7 +220,7 @@ mod tokio_attempt {
                 //println!("instruction count: {}", cpu.instruction_count);
                 //println!("cycle count: {}", cpu.cycles);
 
-                tokio::task::yield_now().await;
+                //tokio::task::yield_now().await;
             }
         });
     }
@@ -318,7 +319,7 @@ mod async_std_attempt {
                 //println!("instruction count: {}", cpu.instruction_count);
                 //println!("cycle count: {}", cpu.cycles);
 
-                async_std::task::yield_now().await;
+                //async_std::task::yield_now().await;
             }
         });
         async_std::task::block_on(task);
@@ -361,57 +362,57 @@ mod enum_attempt {
             }
         }
         /*
-        void CPU::executeInstructionCycle() {
-          if(cycle == 1) {
-            opcode = readMemory(PC++);
-            cycle = 2;
-            return;
-          }
-          if(FlagM)
-          switch(opcode) {  //8-bit accumulator instructions
-          case 0xb9:
-            switch(cycle) {
-            case 2:
-              switch(subcycle) {
-              case 1:
-                subcycle = 2;
-                return;
-              case 2:
-                address = readMemory(PC++);
-                subcycle = 1;
-                return;
-              }
-            case 3:
-              switch(subcycle) {
-              case 1:
-                subcycle = 2;
-                return;
-              case 2:
-                address = readMemory(PC++) | address << 8;
-                subcycle = 1;
-                return;
-              }
-            case 4:
-              //possible penalty cycle when crossing 8-bit page boundaries:
-              if(address >> 8 != address + Y >> 8) {
-                return;
-              }
-              cycle++;  //cycle 4 not needed; fall through to cycle 5
-            case 5:
-              switch(subcycle) {
-              case 1:
-                subcycle = 2;
-                return;
-              case 2:
-                A = readMemory(address + Y);
-                subcycle = 1;
-                cycle = 0;  //end of instruction; start a new instruction next time
-                return;
-              }
-            }
-          }
-        }
-*/
+                void CPU::executeInstructionCycle() {
+                  if(cycle == 1) {
+                    opcode = readMemory(PC++);
+                    cycle = 2;
+                    return;
+                  }
+                  if(FlagM)
+                  switch(opcode) {  //8-bit accumulator instructions
+                  case 0xb9:
+                    switch(cycle) {
+                    case 2:
+                      switch(subcycle) {
+                      case 1:
+                        subcycle = 2;
+                        return;
+                      case 2:
+                        address = readMemory(PC++);
+                        subcycle = 1;
+                        return;
+                      }
+                    case 3:
+                      switch(subcycle) {
+                      case 1:
+                        subcycle = 2;
+                        return;
+                      case 2:
+                        address = readMemory(PC++) | address << 8;
+                        subcycle = 1;
+                        return;
+                      }
+                    case 4:
+                      //possible penalty cycle when crossing 8-bit page boundaries:
+                      if(address >> 8 != address + Y >> 8) {
+                        return;
+                      }
+                      cycle++;  //cycle 4 not needed; fall through to cycle 5
+                    case 5:
+                      switch(subcycle) {
+                      case 1:
+                        subcycle = 2;
+                        return;
+                      case 2:
+                        A = readMemory(address + Y);
+                        subcycle = 1;
+                        cycle = 0;  //end of instruction; start a new instruction next time
+                        return;
+                      }
+                    }
+                  }
+                }
+        */
         pub fn execute_instruction(&mut self) -> bool {
             //println!("execute instruction");
             if self.cycle == 1 {
@@ -426,65 +427,65 @@ mod enum_attempt {
                 0xb9 => {
                     //println!("decode 0xb9");
                     match self.cycle {
-                    2 => match self.subcycle {
-                        1 => {
-                            //println!("waiting");
-                            self.subcycle = 2;
-                            return false;
+                        2 => match self.subcycle {
+                            1 => {
+                                //println!("waiting");
+                                self.subcycle = 2;
+                                return false;
+                            }
+                            2 => {
+                                //println!("reading byte");
+                                self.address = self.read_memory(self.pc) as u16;
+                                self.pc += 1;
+                                self.subcycle = 1;
+                                self.cycle = 3;
+                                return false;
+                            }
+                            _ => {}
+                        },
+                        3 => match self.subcycle {
+                            1 => {
+                                //println!("waiting to read other byte");
+                                self.subcycle = 2;
+                                return false;
+                            }
+                            2 => {
+                                //println!("reading other byte");
+                                self.address = self.read_memory(self.pc) as u16 | self.address << 8;
+                                self.pc += 1;
+                                self.subcycle = 1;
+                                self.cycle = 4;
+                                return false;
+                            }
+                            _ => {}
+                        },
+                        4 => {
+                            // Taking a bit of liberty here
+                            //println!("cycle 4");
+                            self.cycle += 1;
+                            if self.address >> 8 != self.address + self.y as u16 >> 8 {
+                                return false;
+                            }
                         }
-                        2 => {
-                            //println!("reading byte");
-                            self.address = self.read_memory(self.pc) as u16;
-                            self.pc += 1;
-                            self.subcycle = 1;
-                            self.cycle = 3;
-                            return false;
-                        }
+                        5 => match self.subcycle {
+                            1 => {
+                                //println!("cycle 5 wait");
+                                self.subcycle = 2;
+                                return false;
+                            }
+                            2 => {
+                                //println!("cycle 5 done");
+                                self.a = self.read_memory(self.address + self.y as u16);
+                                self.subcycle = 1;
+                                self.cycle = 1;
+                                self.instruction_count += 1;
+                                return true;
+                            }
+                            _ => {}
+                        },
                         _ => {}
                     }
-                    3 => match self.subcycle {
-                        1 => {
-                            //println!("waiting to read other byte");
-                            self.subcycle = 2;
-                            return false;
-                        }
-                        2 => {
-                            //println!("reading other byte");
-                            self.address = self.read_memory(self.pc)  as u16| self.address << 8;
-                            self.pc += 1;
-                            self.subcycle = 1;
-                            self.cycle = 4;
-                            return false;
-                        }
-                        _ => {}
-                    }
-                    4 => {
-                        // Taking a bit of liberty here
-                        //println!("cycle 4");
-                        self.cycle += 1;
-                        if self.address >> 8 != self.address + self.y as u16 >> 8 {
-                            return false;
-                        }
-                    }
-                    5 => match self.subcycle {
-                        1 => {
-                            //println!("cycle 5 wait");
-                            self.subcycle = 2;
-                            return false;
-                        }
-                        2 => {
-                            //println!("cycle 5 done");
-                            self.a = self.read_memory(self.address + self.y as u16);
-                            self.subcycle = 1;
-                            self.cycle = 1;
-                            self.instruction_count += 1;
-                            return true;
-                        }
-                        _ => {}
-
-                    }
-                    _ => {}
-                }}
+                }
                 _ => {
                     // do nothing
                 }
@@ -514,13 +515,152 @@ mod enum_attempt {
     pub fn main(iters: usize) {
         let mut cpu = CPU::new();
         for _ in 0..iters {
-            while cpu.execute_instruction() == false {};
+            while cpu.execute_instruction() == false {}
             //println!("ran for 1 instruction");
             //println!("instruction count: {}", cpu.instruction_count);
             //println!("cycle count: {}", cpu.cycles);
         }
     }
 }
+
+mod generator_attempt {
+    use super::*;
+    use std::ops::{Generator, GeneratorState};
+    use std::pin::Pin;
+
+    // Stolen from: https://github.com/kylewlacy/lochnes/blob/fbd3824b6be3362f2bc117f7eef9e4e9f34c2918/src/gen_utils.rs
+    #[macro_export]
+    macro_rules! yield_all {
+        ($gen: expr) => {{
+            use std::ops::{Generator, GeneratorState};
+            use std::pin::Pin;
+
+            let mut gen = $gen;
+            loop {
+                match Pin::new(&mut gen).resume(()) {
+                    GeneratorState::Yielded(yielded) => {
+                        yield yielded;
+                    }
+                    GeneratorState::Complete(result) => {
+                        break result;
+                    }
+                }
+            }
+        }};
+    }
+
+    struct CPU {
+        pc: u16,
+        x: u8,
+        y: u8,
+        a: u8,
+        apu_counter: u32,
+        cycles: u32,
+        instruction_count: u32,
+        mem: [u8; MEM_SIZE],
+    }
+
+    impl CPU {
+        pub fn new() -> CPU {
+            CPU {
+                pc: 0,
+                x: 0,
+                y: 0,
+                a: 0,
+                apu_counter: 0,
+                mem: [0xb9; MEM_SIZE],
+                cycles: 0,
+                instruction_count: 0,
+            }
+        }
+        /*
+        void CPU::executeInstruction() {
+          opcode = readMemory(PC++);
+          if(FlagM)
+          switch(opcode) {  //8-bit accumulator instructions
+          case 0xb9:
+            address = readMemory(PC++);
+            address = readMemory(PC++) | address << 8;
+            if(address >> 8 != address + Y >> 8) wait(6);
+            A = readMemory(address + Y);
+          }
+        }
+        */
+        pub fn execute_instruction<'a>(
+            &'a mut self,
+        ) -> impl Generator<Yield = (), Return = ()> + 'a {
+            move || {
+                //println!("execute instruction");
+                let opcode = yield_all!(self.read_memory(self.pc));
+                self.pc += 1;
+                match opcode {
+                    // 8-bit accumulator instructions
+                    0xb9 => {
+                        //println!("found 0xb9 instruction");
+                        let address: u16 = yield_all!(self.read_memory(self.pc)).into();
+                        self.pc += 1;
+                        let address: u16 =
+                            yield_all!(self.read_memory(self.pc)) as u16 | address << 8;
+                        self.pc += 1;
+                        if address >> 8 != address + self.y as u16 >> 8 {
+                            yield_all!(self.wait(6));
+                        }
+                        self.a = yield_all!(self.read_memory(address + self.y as u16));
+                    }
+                    _ => {
+                        // do nothing
+                    }
+                }
+                self.instruction_count += 1;
+                yield;
+            }
+        }
+
+        fn read_memory<'a>(
+            &'a mut self,
+            addr: u16,
+        ) -> impl Generator<Yield = (), Return = u8> + 'a {
+            move || {
+                //println!("read_memory");
+                yield_all!(self.wait(2));
+                let data = self.mem[addr as usize];
+                yield_all!(self.wait(4));
+                data
+            }
+        }
+
+        fn wait<'a>(
+            &'a mut self,
+            clock_cycles: u32,
+        ) -> impl Generator<Yield = (), Return = ()> + 'a {
+            move || {
+                //println!("wait for {} cycles", clock_cycles);
+                self.apu_counter += clock_cycles;
+                while self.apu_counter > 0 {
+                    yield;
+                    self.apu_counter -= 1;
+                    self.cycles += 1;
+                    //println!("total cycles {}", self.cycles);
+                }
+            }
+        }
+    }
+
+    pub fn main(iters: usize) {
+        let mut cpu = CPU::new();
+        for _ in 0..iters {
+            match Pin::new(&mut CPU::execute_instruction(&mut cpu)).resume(()) {
+                GeneratorState::Yielded { .. } => {
+                    //println!("Instruction compeleted");
+                }
+                GeneratorState::Complete { .. } => {
+                    println!("generator shouldn't return");
+                }
+            }
+        }
+    }
+}
+
 mod null_attempt {
     use super::*;
 
@@ -639,6 +779,9 @@ fn main() {
         }),
         timeit!("async-std", {
             async_std_attempt::main(count);
+        }),
+        timeit!("generator", {
+            generator_attempt::main(count);
         }),
         timeit!("enum", {
             enum_attempt::main(count);
